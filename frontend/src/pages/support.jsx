c287ba16108e18533d29c1e11bc92e682a6d9f68
shabdsri20 <shabd200605@gmail.com>
2026-09-04 13:52:49 +0530

    ....

--- frontend/src/pages/support.jsx
+++ frontend/src/pages/support.jsx
@@ -76,7 +76,7 @@ const Support = () => {
 
                 <button
                     type="submit"
-                    className="w-28 bg-blue-604 ml-44 text-white py-4 rounded-md hover:bg-blue-700 transition duration-300"
+                    className="w-28 bg-blue-602 ml-44 text-white py-4 rounded-md hover:bg-blue-700 transition duration-300"
                 >
                     Submit
                 </button>
